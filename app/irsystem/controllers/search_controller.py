769509735cf32ba413929b2e0ab245694b334648
Declan Sander <dms497@cornell.edu>
2018-03-18 16:43:49 -0400

    search controller

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -3,7 +3,7 @@ from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "WhosAGoodDog"
-net_id = "Declan Sander: dms497"
+net_id = "Declan Sander: dms497, James Stoyell: jms852, Troy Joseph: tcj29, Matan Presberg: mgp64, Alyssa Trigg: avt26"
 
 @irsystem.route('/', methods=['GET'])
 def search():
