175588b381647be4ed2298949c4478264263c838
Declan Sander <dms497@cornell.edu>
2018-03-19 22:54:23 -0400

    base components

---
+++ frontend/js/app.js
@@ -0,0 +1,5 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom';
+import Base from 'components/Base';
+
+ReactDOM.render(<Base />, document.getElementById('react'));
---
+++ frontend/js/components/Base.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider>
+      </Provider>
+    );
+  }
+}
