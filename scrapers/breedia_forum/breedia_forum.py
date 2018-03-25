c3c73f4b1cb2577ac8267a76039561c711a3bfe0
James Stoyell <jms852@cornell.edu>
2018-03-25 16:55:27 -0400

    parse all pages of site

--- scrapers/breedia_forum/breedia_forum.py
+++ scrapers/breedia_forum/breedia_forum.py
@@ -8,7 +8,7 @@ import shelve
 import json
 from tqdm import tqdm
 
-RATE_LIMIT = 5
+RATE_LIMIT = 3
 BASE_URL = "http://www.forum.breedia.com/"
 BREEDS_ROOT = "categories/dog-breed-forums.3/"
 last_request = 0
@@ -18,16 +18,20 @@ data = {}
 def url_get(url):
     url = str(url)
     if url in cache:
-        return cache[url]
+        data = cache[url]
+        if type(data) == unicode:
+            return BSoup(data, 'html.parser')
+        else:
+            return data
     global last_request
     diff = time.time() - last_request
     if diff < 1/RATE_LIMIT:
         time.sleep(1/RATE_LIMIT - diff)
 
     last_request = time.time()
-    data = BSoup(requests.get(url).text, 'html.parser')
+    data = requests.get(url).text
     cache[url] = data
-    return data
+    return BSoup(data, 'html.parser')
 
 
 if __name__ == "__main__":
@@ -46,22 +50,31 @@ if __name__ == "__main__":
 
                     for breed_link in breeds_page.find_all(class_="nodeTitle"):
                         breed = breed_link.text
+                        # if breed[0].lower() in ['a', 'b', 'c']:
+                        #     continue
                         print breed
 
                         breed_messages = []
 
                         breed_ref = breed_link.find('a').get('href')
-
                         breed_page = url_get(BASE_URL + breed_ref)
-                        for thread_link in tqdm(breed_page.find_all(class_="PreviewTooltip")):
-                            thread_ref = thread_link.get('href')
-                            thread_page = url_get(BASE_URL + thread_ref)
+                        pages_obj = breed_page.find(class_="pageNavHeader")
+                        if pages_obj:
+                            num_pages = int(pages_obj.text[-1])
+                        else:
+                            num_pages = 1
+                        for page_num in tqdm(range(1, num_pages+1)):
+                            if page_num != 1:
+                                breed_page = url_get(BASE_URL + breed_ref + "page-" + str(page_num))
+                            for thread_link in breed_page.find_all(class_="PreviewTooltip"):
+                                thread_ref = thread_link.get('href')
+                                thread_page = url_get(BASE_URL + thread_ref)
+                                if "stud" in (BASE_URL + thread_ref).lower():
+                                    print BASE_URL + thread_ref
 
-                            for message in thread_page.find_all(class_="messageText"):
-                                breed_messages.append(message.text.strip())
+                                for message in thread_page.find_all(class_="messageText"):
+                                    breed_messages.append(message.text.strip())
 
-                            data[breed] = breed_messages
-                    with open("output.json", 'w') as f:
-                        f.write(json.dumps(data, indent=2))
-                                
-            sys.exit()
+                                data[breed] = breed_messages
+                        with open("output.json", 'w') as f:
+                            f.write(json.dumps(data, indent=2))
