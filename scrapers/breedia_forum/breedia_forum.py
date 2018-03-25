c904630f55c7260367e772e7564479d1c33fa632
James Stoyell <jms852@cornell.edu>
2018-03-25 19:32:54 -0400

    done breedia scraping

--- scrapers/breedia_forum/breedia_forum.py
+++ scrapers/breedia_forum/breedia_forum.py
@@ -13,27 +13,29 @@ BASE_URL = "http://www.forum.breedia.com/"
 BREEDS_ROOT = "categories/dog-breed-forums.3/"
 last_request = 0
 cache = shelve.open("cache.data")
-data = {}
+with open("output.json", 'r') as f:
+    data = json.loads(f.read())
+DOG_BREED_LIST = []
 
 def url_get(url):
     url = str(url)
     if url in cache:
-        data = cache[url]
-        if type(data) == unicode:
-            return BSoup(data, 'html.parser')
+        html_text = cache[url]
+        if type(html_text) == unicode:
+            return BSoup(html_text, 'html.parser')
         else:
-            return data
+            return html_text
     global last_request
     diff = time.time() - last_request
     if diff < 1/RATE_LIMIT:
         time.sleep(1/RATE_LIMIT - diff)
 
     last_request = time.time()
-    data = requests.get(url).text
-    cache[url] = data
-    return BSoup(data, 'html.parser')
+    html_text = requests.get(url).text
+    cache[url] = html_text
+    return BSoup(html_text, 'html.parser')
 
-
+n = 0
 if __name__ == "__main__":
     base_forum = url_get(BASE_URL + BREEDS_ROOT)
 
@@ -52,7 +54,9 @@ if __name__ == "__main__":
                         breed = breed_link.text
                         # if breed[0].lower() in ['a', 'b', 'c']:
                         #     continue
-                        if breed.lower() != "akita":
+                        n += 1
+                        if breed in data:
+                            print breed, "already done"
                             continue
                         print breed
 
@@ -80,11 +84,18 @@ if __name__ == "__main__":
                                 for thread_page_num in xrange(1, num_pages+1):
                                     if thread_page_num != 1:
                                         thread_page = url_get(BASE_URL + thread_ref + "page-" + str(thread_page_num))
-                                        print BASE_URL + thread_ref + "page-" + str(thread_page_num)
 
                                     for message in thread_page.find_all(class_="messageText"):
                                         breed_messages.append(message.text.strip())
 
-                                data[breed] = breed_messages
-                        with open("output.json", 'w') as f:
-                            f.write(json.dumps(data, indent=2))
+                        data[breed] = breed_messages
+                        not_done = True
+                        while not_done:
+                            try:
+                                with open("output.json", 'w') as f:
+                                    f.write(json.dumps(data, indent=2))
+                                not_done = False
+                            except KeyboardInterrupt:
+                                pass
+
+print n
