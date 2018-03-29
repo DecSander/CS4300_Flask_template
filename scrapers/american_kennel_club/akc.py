edc956a07aa747f3d8553cf989904cfb757d527b
Matan Presberg <mgp64@cornell.edu>
2018-03-29 17:52:15 -0400

    progress on akc scraper, just need to finish parsing each breed page

--- scrapers/american_kennel_club/akc.py
+++ scrapers/american_kennel_club/akc.py
@@ -20,6 +20,7 @@ def url_get(url):
     if url in cache:
         data = cache[url]
         return BS(data, 'html.parser')
+    #print('new call')
     global last_request
     diff = time.time() - last_request
     if diff < 1/RATE_LIMIT:
@@ -32,7 +33,48 @@ def url_get(url):
     return BS(data, 'html.parser')
 
 if __name__ == "__main__":
-    breeds_home = url_get(BASE_URL + BREEDS_ROOT)
+    data = {}
+    for page in range(1,24):
+        breeds_page = url_get(BASE_URL + BREEDS_ROOT + 'page/' + str(page))
+        print("Page: " + str(page))
+        for dog_grid_el in tqdm(breeds_page.find_all('div', class_='grid-col')):
+            links = dog_grid_el.find_all('a', class_='d-block relative', href=True)    
+            dog_data = {}
+            if len(links) > 0:
+                page_url = links[0]['href']
+                breed_page = url_get(page_url)
+                breed_name = page_url.split("/")[-2]
+            else:
+                continue 
 
-    for link in tqdm(breeds_home.find_all('a')): 
-        print(link)
+            attribute_list = breed_page.find(class_='attribute-list')
+            about_the_breed = breed_page.find(class_='breed-info__content-wrap')
+            #print("########################")
+            #print(breed_name)
+
+            
+            text_list = attribute_list.find_all(class_='attribute-list__text')
+            for i in range(len(text_list)):
+                if i % 2 == 0:
+                    dog_data[text_list[i].get_text()] = text_list[i + 1].get_text()
+            #print(attribute_list.prettify())
+                
+            #print('-------')
+            dog_data['About'] = about_the_breed.get_text()
+
+            fact_list = breed_page.find_all(class_='fact-slider__slide-content')
+
+            facts = []
+            for fact in fact_list:
+                facts.append(fact.get_text().strip())
+
+            dog_data['Facts'] = facts
+
+
+            care_list = breed_page.find_all(class_='tabs__tab-panel-content')            
+            #for care in care_list:
+                #print(care.prettify())
+
+            data[breed_name] = dog_data
+
+   # print(json.dumps(data, indent=2, separators=(',', ': ')))
