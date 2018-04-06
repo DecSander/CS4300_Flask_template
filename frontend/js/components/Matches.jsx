bfe69e71e78d7b2bd0be421562f42f8b5f3ca6ce
Declan Sander <dms497@cornell.edu>
2018-04-05 22:59:42 -0500

    dialog stuff

--- frontend/js/components/Matches.jsx
+++ frontend/js/components/Matches.jsx
@@ -6,6 +6,8 @@ import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import CircularProgress from 'material-ui/CircularProgress';
+import Carousel from 'nuka-carousel';
+
 import { capitalizeFirstLetter } from 'infra/utils';
 
 function mapStateToProps({ liked, likedLoading }) {
@@ -16,21 +18,33 @@ class Matches extends React.Component {
 
   state = {
     modalOpen: false,
-    selectedBreed: null
+    selectedBreed: null,
+    selectedNumber: 0
   }
 
-  handleOpen = (breed) => {
+  handleOpen = (breed, i) => {
     this.setState({
       modalOpen: true,
-      selectedBreed: breed
+      selectedBreed: breed,
+      selectedNumber: i
     });
   }
 
   handleClose = () => {
     this.setState({
       modalOpen: false,
-      selectedBreed: null
+      selectedBreed: null,
+      selectedNumber: 0
     });
+  }
+
+  buildSlideshow = () => {
+    const { selectedBreed } = this.state;
+    return (
+      <Carousel slideWidth="400px">
+        {selectedBreed.img.map(image => <img style={{height: '300px', width: '400px'}} src={image} />)}
+      </Carousel>
+    );
   }
 
   buildDialog = () => {
@@ -40,7 +54,7 @@ class Matches extends React.Component {
     } else {
       return (
         <Dialog title={capitalizeFirstLetter(selectedBreed.name)} modal={false} open={modalOpen} onRequestClose={this.handleClose}>
-          <img style={{height: '300px', width: '200px'}} src={selectedBreed.img.get(0)} alt={selectedBreed.name}/>
+          {this.buildSlideshow()}
           <br />
           {'Breed Description here'}
         </Dialog>
@@ -58,7 +72,7 @@ class Matches extends React.Component {
       return liked.map((breed, i) =>
         <Col lg={4} xs={12}>
           <Card style={{margin: 'auto', marginTop: '20px'}} key={`match-${breed.name}-${i}`}>
-            <div onClick={() => this.handleOpen(breed)}>
+            <div onClick={() => this.handleOpen(breed, i)}>
               <CardMedia style={{width: 400, height: 300}} overlay={<CardTitle title={capitalizeFirstLetter(breed.name)} />}>
                 <img style={{width: 400, height: 300}} src={breed.img.get(0)} alt={breed.name}/>
               </CardMedia>
