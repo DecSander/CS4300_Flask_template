111360988cf8db2f15475e395d94597b9c4a63cd
Declan Sander <dms497@cornell.edu>
2018-04-09 21:22:09 -0400

    bundle

--- frontend/js/components/Home.jsx
+++ frontend/js/components/Home.jsx
@@ -51,12 +51,12 @@ class Home extends React.Component {
       <Container fluid>
         <Row>
           <Col offset={{lg: 4}} lg={4} xs={12}>
-            <div style={{textAlign: 'center', fontWeight: '200', fontFamily: 'roboto', color: 'blue'}}>
-              <h1 style={{fontSize: '56px'}}>Who's A Good Dog?</h1>
-              <h3 style={{fontSize: '30px'}}>Find out which dog you should get</h3>
-              <TextField floatingLabelStyle={{color: 'blue'}} style={{width: '500px', fontSize: '30px'}} floatingLabelText="Search" onChange={(e, v) => updatePreference('keywords', v)} />
+            <div style={{textAlign: 'center', fontWeight: '200', fontFamily: 'roboto', color: 'black'}}>
+              <h1 style={{backgroundColor: 'white', fontSize: '56px'}}>Who's A Good Dog?</h1>
+              <h3 style={{backgroundColor: 'white', fontSize: '30px'}}>Find out which dog you should get</h3>
+              <TextField floatingLabelStyle={{color: 'white'}} style={{width: '500px', fontSize: '30px'}} floatingLabelText="Search" onChange={(e, v) => updatePreference('keywords', v)} />
               <br />
-              <RaisedButton primary={true} style={{marginRight: '20px'}} onClick={this.submitWithPrefs}>Select More Prefs</RaisedButton>
+              <RaisedButton primary={true} style={{marginRight: '20px'}} onClick={this.submitWithPrefs}>More Preferences</RaisedButton>
               <RaisedButton secondary={true} onClick={this.submitNoPrefs}>Submit</RaisedButton>
             </div>
           </Col>
