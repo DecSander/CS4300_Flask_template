93ec92b1ddaf265ef45bc8c0eecb1d5f4030ede9
James Stoyell <jms852@cornell.edu>
2018-04-10 21:28:38 -0400

    more data changes

--- scrapers/data_creator.py
+++ scrapers/data_creator.py
@@ -14,7 +14,7 @@ wagwalking_data = {k.lower(): v for k, v in wagwalking_data.items()}
 breedia_data = json.load(open("breedia_forum/output.json", 'r'))
 breedia_data = {k.lower(): v for k, v in breedia_data.items()}
 
-
+print wagwalking_data['rottweiler']['ratings']
 
 final_dataset = {}
 
@@ -24,9 +24,9 @@ akc_numerical = ["temperament/demeanor", "height", "trainability", "energy level
                  "grooming frequency", "life expectancy", "weight", "shedding", "akc breed popularity", "group"]
 
 ww_textual = ["temperament", "overview", "appearance", "texture",
-              "reviews", "ancestry", "maintenance", "concerns", "history"]
+              "reviews", "ancestry", "maintenance", "concerns", "history", "activity"]
 ww_numerical = ["size", "activity_minutes", "grooming_freq", "lifespan", "ratings", "food_monthly_cost",
-                "height", "num_ratings", "activity_level", "walk_miles", "coat_length", "activity"]
+                "height", "num_ratings", "activity_level", "walk_miles", "coat_length"]
 
 def depercentify(s):
     return int(s[:-1])/100.0
@@ -41,6 +41,19 @@ def is_num(x):
     except ValueError:
         return False
 
+def av(v1, v2, v2_num_ratings=100):
+    if v1 is None or v1 < 0:
+        return v2
+    elif v2 is None or v2 < 0:
+        return v1
+    else:
+        if v2_num_ratings >= 100:
+            return (v1 + v2)/2
+        else:
+            v2_weight = v2_num_ratings/100
+            v1_weight = 1-v2_weight
+            return v1 * v1_weight + v2*v2_weight
+
 for doggo in akc_data:
     print doggo
     if doggo in ww_matches:
@@ -48,8 +61,17 @@ for doggo in akc_data:
         ww_text_data = {k: ww_info[k] for k in ww_textual}
         ww_num_data = {k: ww_info[k] for k in ww_numerical}
     else:
-        ww_text_data = None
-        ww_num_data = None
+        ww_text_data = defaultdict(lambda : None)
+        ww_num_data = defaultdict(lambda : None)
+
+    if ww_num_data['ratings'] is None:
+        ww_num_data['ratings'] = {
+            "Friendliness": -1, 
+            "Grooming": -1, 
+            "Trainability": -1, 
+            "Energy": -1,
+            "Health": -1
+        }
 
     if doggo in b_matches:
         b_info = breedia_data[b_matches[doggo]]
@@ -112,16 +134,32 @@ for doggo in akc_data:
     akc_text_data = {k: akc_info[k] for k in akc_textual}
     akc_num_data = {k: akc_info[k] for k in akc_numerical}
 
+    structured_data = {
+        "temperament" : av(akc_num_data["temperament/demeanor"], ww_num_data["ratings"]["Friendliness"]/5.0, ww_num_data["num_ratings"]), 
+        "height" : av(akc_num_data["height"], ww_num_data["height"]), 
+        "trainability" : av(akc_num_data["trainability"], ww_num_data["ratings"]["Trainability"]/5.0, ww_num_data["num_ratings"]), 
+        "energy level" : av(akc_num_data["energy level"], ww_num_data["ratings"]["Energy"]/5.0, ww_num_data["num_ratings"]),
+        "grooming frequency" : akc_num_data["grooming frequency"], 
+        "lifespan" : av(akc_num_data["life expectancy"], ww_num_data["lifespan"]), 
+        "weight" : av(akc_num_data["weight"], ww_num_data["size"])/2, 
+        "shedding" : akc_num_data["shedding"], 
+        "akc breed popularity" : akc_num_data["akc breed popularity"], 
+        "group" : akc_num_data["group"],
+        "activity_minutes" : ww_num_data["activity_minutes"],
+        "grooming_freq_text" : ww_num_data["grooming_freq"],
+        "health": ww_num_data["ratings"]["Health"]/5.0,
+        "food_monthly_cost" : ww_num_data["food_monthly_cost"],
+        "activity_level" : ww_num_data["activity_level"],
+        "walk_miles" : ww_num_data["walk_miles"],
+        "coat_length" : ww_num_data["coat_length"],
+    }
+
     final_dataset[doggo] = {"text": {
         "breedia": b_text_data,
         "wagwalking": ww_text_data,
         "akc": akc_text_data,
     },
-        "structured": {
-        "breedia": None,
-        "wagwalking": ww_num_data,
-        "akc": akc_num_data,
-    }
+        "structured": structured_data
     }
 
 print len(final_dataset)
