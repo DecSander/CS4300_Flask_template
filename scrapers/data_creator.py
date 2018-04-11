86b97f56e53a867f0de4d76b79cf59bdec5d1518
James Stoyell <jms852@cornell.edu>
2018-04-10 20:34:07 -0400

    example data, work on dataset

--- scrapers/data_creator.py
+++ scrapers/data_creator.py
@@ -1,5 +1,6 @@
+from __future__ import division
 import json
-import pyperclip
+import re
 from collections import defaultdict
 
 ww_matches = json.load(open("wwpairs.json", 'r'))
@@ -26,6 +27,12 @@ ww_textual = ["temperament", "overview", "appearance", "texture",
 ww_numerical = ["size", "activity_minutes", "grooming_freq", "lifespan", "ratings", "food_monthly_cost",
                 "height", "num_ratings", "activity_level", "walk_miles", "coat_length", "activity"]
 
+def depercentify(s):
+    return int(s[:-1])/100.0
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
 for doggo in akc_data:
     print doggo
     if doggo in ww_matches:
@@ -44,6 +51,44 @@ for doggo in akc_data:
 
     akc_info = defaultdict(lambda : None)
     akc_info.update(akc_data[doggo])
+    if akc_info["Shedding"] is not None:
+        akc_info["Shedding"] = depercentify(akc_info["Shedding"])
+
+    if akc_info["Temperament/Demeanor"] is not None:
+        akc_info["Temperament/Demeanor"] = depercentify(akc_info["Temperament/Demeanor"])
+
+    if akc_info["Height"] is not None:
+        height_re = re.compile(r"(\d+)-(\d+)")
+        heights = map(float, flatten(height_re.findall(akc_info["Height"])))
+        akc_info["Height"] = sum(heights)/len(heights)
+
+    if akc_info["Energy Level"] is not None:
+        akc_info["Energy Level"] = depercentify(akc_info["Energy Level"])
+
+    if akc_info["Life Expectancy:"] is not None:
+        life_re = re.compile(r"(\d+)-(\d+)")
+        akc_info["Life Expectancy:"] = sum(map(float, flatten(life_re.findall(akc_info["Life Expectancy:"]))))/2.0
+
+    if akc_info["AKC Breed Popularity:"] is not None:
+        pop_re = re.compile(r"(\d+) of (\d+)")
+        try:
+            rank, total = map(int, flatten(pop_re.findall(akc_info["AKC Breed Popularity:"])))
+            akc_info["AKC Breed Popularity:"] = rank/total
+        except ValueError:
+            akc_info["AKC Breed Popularity:"] = None
+
+    if akc_info["Weight"] is not None:
+        Weight_re = re.compile(r"(\d+)-(\d+)")
+        Weights = map(float, flatten(Weight_re.findall(akc_info["Weight"])))
+        akc_info["Weight"] = sum(Weights)/len(Weights)
+
+    if akc_info["Trainability"] is not None:
+        akc_info["Trainability"] = depercentify(akc_info["Trainability"])
+
+    if akc_info["Grooming Frequency"] is not None:
+        akc_info["Grooming Frequency"] = depercentify(akc_info["Grooming Frequency"])
+
+
     akc_text_data = {k: akc_info[k] for k in akc_textual}
     akc_num_data = {k: akc_info[k] for k in akc_numerical}
 
