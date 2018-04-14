7ee998bfc8d9b51de79efd3455d522b3f74e678b
Declan Sander <dms497@cornell.edu>
2018-04-14 14:21:16 -0400

    fixes

--- app/irsystem/src/structured_compare.py
+++ app/irsystem/src/structured_compare.py
@@ -1,17 +1,15 @@
 from __future__ import division
 import json
 import os
-from collections import namedtuple
-import re
 import sys
-import time
 
 DATA_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "final_dataset.json")
 # This is here in case all the weights are 0, we don't want to just fail
 WEIGHT_EPSILON = .00001
 
 with open(DATA_FILE, 'r') as f:
-    doggo_data = {k:v["structured"] for k,v in json.load(f).iteritems()}
+    doggo_data = {k: v["structured"] for k, v in json.load(f).iteritems()}
+
 
 def _scale_val(val_name):
     mi = sys.maxint
@@ -24,7 +22,8 @@ def _scale_val(val_name):
 
     for dog in doggo_data:
         if doggo_data[dog][val_name] is not None:
-            doggo_data[dog][val_name] = (doggo_data[dog][val_name] - mi)/(ma - mi)
+            doggo_data[dog][val_name] = (doggo_data[dog][val_name] - mi) / (ma - mi)
+
 
 _scale_val("weight")
 _scale_val("lifespan")
@@ -34,9 +33,10 @@ _scale_val("coat_length")
 _scale_val("activity_minutes")
 _scale_val("walk_miles")
 
+
 def _score(preferences, dog):
-    total_weight = sum(v["importance"]+WEIGHT_EPSILON for k,v in preferences.items() if doggo_data[dog][k] is not None)
-    missing_weight = sum(v["importance"]+WEIGHT_EPSILON for k,v in preferences.items()) - total_weight
+    total_weight = sum(v["importance"] + WEIGHT_EPSILON for k, v in preferences.items() if doggo_data[dog][k] is not None)
+    missing_weight = sum(v["importance"] + WEIGHT_EPSILON for k, v in preferences.items()) - total_weight
     score = 0
     contributions = {}
     for p in preferences:
@@ -49,9 +49,10 @@ def _score(preferences, dog):
     confidence = missing_weight / total_weight
     return {"score": score, "confidence": confidence, "contributions": contributions}
 
+
 def structured_score(preferences):
     return {d: _score(preferences, d) for d in doggo_data}
 
-    
+
 if __name__ == "__main__":
     print doggo_data['affenpinscher']
